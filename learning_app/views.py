ae3e07ebe9cf12045e155c7c05002f0b6160c9c3
Momo <salmamomo85@gmail.com>
2025-02-01 00:52:27 +0600

    first commit

--- learning_app/views.py
+++ learning_app/views.py
@@ -7,6 +7,31 @@ from django.contrib import messages
 def home(request):
     return render(request,'home.html')
 
+# def login(request):
+#     try:
+#         if request.method=="post":
+#             username=str(request.post.get('username'))
+#             password=str(request.post.get('password'))
+
+#             return HttpResponseRedirect ('/home/')
+#         else:
+#             pass
+#     return render(request,'login_page.html')
+
+# def registration(request):
+#     try:
+#         if request.method=="post":
+#             username=str(request.post.get('username'))
+#             password=str(request.post.get('password'))
+#             conform password=str(request.post.get('password'))
+#             email=str(request.post.get('email'))
+
+#              return HttpResponseRedirect ('/home/')
+#         else:
+#             pass
+#     return render(request,'registration_page.html')
+
+
 def login(request):
     return render(request,'login_page.html')
 
